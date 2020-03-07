397d5e960c42706f8ec4739d2c8ac38082a5aa2d
Rodrigo Pereira <rodrigo55pereira@gmail.com>
2020-03-07 14:48:42 -0300

    Finalização do CRUD de categories

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,16 +16,20 @@ namespace Shop.Controllers
     {
         [HttpGet]
         [Route("")]
-        public async Task<ActionResult<List<Category>>> Get()
+        public async Task<ActionResult<List<Category>>> Get([FromServices]DataContext context)
         {
-            return new List<Category>();
+            // AsNoTracking -> recupera a categoria de modo mais rápido possível (Só leitura). 
+            //ToListAsync - Executa a consulta no banco de dados de fato. 
+            var categories = await context.Categories.AsNoTracking().ToListAsync();
+            return Ok(categories);
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public async Task<ActionResult<Category>> GetById(int id)
+        public async Task<ActionResult<Category>> GetById(int id, [FromServices] DataContext context)
         {
-            return new Category();
+            var category = await context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(category);
         }
 
         [HttpPost]
